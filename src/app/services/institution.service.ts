57abc9f02e3c713cc5f218504a689463ec45e12d
zevania <zevania@u.nus.edu>
2020-09-06 16:27:11 +0800

    Added user and institution services

--- src/app/services/institution.service.ts
+++ src/app/services/institution.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_URL = 'https://localhost:8080/api/user';
+const API_URL = 'https://localhost:8080/api/institution';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -11,16 +11,17 @@ const httpOptions = {
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
+export class InstitutionService {
 
   constructor(private http: HttpClient) { }
 
   updateProfile(data): Observable<any> {
     return this.http.post(API_URL + '/updateProfile', {
       name: data.name,
-      occupation: data.occupation,
+      address: data.address,
       bio: data.bio,
-      country: data.country
+      country: data.country,
+      phone: data.phone
     }, httpOptions);
   }
 
