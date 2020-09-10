6f154896f48b714c012359f342584c129a063c98
zevania <zevania@u.nus.edu>
2020-09-10 22:13:56 +0800

    Updated SDGs

--- src/app/edit-profile/edit-profile.component.ts
+++ src/app/edit-profile/edit-profile.component.ts
@@ -20,7 +20,13 @@ export class EditProfileComponent implements OnInit {
   isUploadPicSuccessful = false;
   errorMsgUploadPic = '';
 
-  SDGs = ['1.abc', '2.def', '3.ghi'];
+  SDGs = ['1. No Poverty', '2. Zero Hunger', '3. Good Health and Well-Being',
+          '4. Quality Education', '5. Gender Equality', '6. Clean Water and Sanitation',
+          '7. Affordable and Clean Energy', '8. Decent Work and Economic Growth',
+          '9. Industry, Innovation, and Infrastructure', '10. Reduced Inequalities',
+          '11. Sustainable Cities and Communities', '12. Responsible Consumption and Production',
+          '13. Climate Action', '14. Life Below Water', '15. Life on Land',
+          '16. Peace, Justice, and Strong Institutions', '17. Partnerships'];
   SDGsMap = [];
   SDGsChecked = [];
 
@@ -57,8 +63,6 @@ export class EditProfileComponent implements OnInit {
         this.SDGsChecked.push(parseInt(x)+1);
       }
     }
-    // this.SDGs = this.SDGsChecked;
-    // this.SDGsChecked = [];
   }
 
   selectImage(event) {
