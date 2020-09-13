4a4f4fa0490c65bb852afb5c28f0b3765b0223e1
Trudy <trudylim98@gmail.com>
2020-09-13 16:09:58 +0800

    institution profile

--- src/app/own-profile/own-profile.component.ts
+++ src/app/own-profile/own-profile.component.ts
@@ -10,12 +10,16 @@ import { TokenStorageService } from '../services/token-storage.service';
 export class OwnProfileComponent implements OnInit {
 
   user: any;
+  isIndividual = false;
   //projectList = Array;
 
   constructor(private authService: AuthService, private tokenStorageService: TokenStorageService) { }
 
   ngOnInit(): void {
     this.user = this.tokenStorageService.getUser();
+    if (this.tokenStorageService.getAccountType() == "user") {
+      this.isIndividual = true;
+    }
     //this.projectList = this.user.projects;
   }
 
