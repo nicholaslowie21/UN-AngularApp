7caa56d9d2dadf1c4327f1de084e2433fdaf3e99
zevania <zevania@u.nus.edu>
2020-09-14 18:20:55 +0800

    Search, promote/demote admins

--- src/app/admin/admin-management/admin-management.component.ts
+++ src/app/admin/admin-management/admin-management.component.ts
@@ -13,21 +13,109 @@ export class AdminManagementComponent implements OnInit {
   regionalAdmins: any;
   adminLeads: any;
 
-  content: any;
+  keyword = '';
+  searchResults: any;
+  isSearchSuccessful: any;
+  errorMsgSearch = '';
 
   constructor(private adminService: AdminService, private tokenStorageService: TokenStorageService) { }
 
   ngOnInit(): void {
-    this.adminService.getAdmins().subscribe(
+    this.loadAdmins();
+    this.loadAdminLeads();
+    this.loadRegionalAdmins();
+  }
+
+  searchUsers(): void {
+    if(this.keyword.length == 0) {
+      this.errorMsgSearch = 'Enter a username';
+      return;
+    }
+    this.adminService.searchUser({username: this.keyword}).subscribe(
       response => {
-        this.admins = response.data.admins;
+        this.searchResults = response.data.users;
+        this.isSearchSuccessful = true;
+      },
+      err => {
+        this.errorMsgSearch = err.error.msg;
+        this.isSearchSuccessful = false;
       }
     )
+  }
+
+  assignAdminLead(userId): void {
+    this.adminService.assignAdminLead({id: userId}).subscribe(
+      response => {
+        alert("User promoted to admin lead!");
+        this.loadAdmins();
+        this.loadAdminLeads();
+        this.loadRegionalAdmins();
+      },
+      err => {
+        alert(err.error.msg);
+      }
+    )
+  }
+
+  assignAdmin(userId): void {
+    this.adminService.assignAdmin({id: userId}).subscribe(
+      response => {
+        alert("User promoted to admin!");
+        this.loadAdmins();
+        this.loadAdminLeads();
+        this.loadRegionalAdmins();
+      },
+      err => {
+        alert(err.error.msg);
+      }
+    )
+  }
+
+  assignRegionalAdmin(userId): void {
+    this.adminService.assignRegionalAdmin({id: userId}).subscribe(
+      response => {
+        alert("User promoted to regional admin!");
+        this.loadAdmins();
+        this.loadAdminLeads();
+        this.loadRegionalAdmins();
+      },
+      err => {
+        alert(err.error.msg);
+      }
+    )
+  }
+
+  assignUser(userId): void {
+    this.adminService.assignUser({id: userId}).subscribe(
+      response => {
+        alert("User demoted to normal user!");
+        this.loadAdmins();
+        this.loadAdminLeads();
+        this.loadRegionalAdmins();
+      },
+      err => {
+        alert(err.error.msg);
+      }
+    )
+  }
+
+  loadAdminLeads(): void {
     this.adminService.getAdminLeads().subscribe(
       response => {
         this.adminLeads = response.data.adminLeads;
       }
     )
+  }
+
+  loadAdmins(): void {
+    this.adminService.getAdmins().subscribe(
+      response => {
+        this.admins = response.data.admins;
+      }
+    )
+  }
+
+  loadRegionalAdmins(): void {
     this.adminService.getRegionalAdmins().subscribe(
       response => {
         this.regionalAdmins = response.data.regionalAdmins;
@@ -35,8 +123,8 @@ export class AdminManagementComponent implements OnInit {
     )
   }
 
-  showAdminLeads(): void {
-    this.content = this.adminLeads;
+  reloadPage(): void {
+    window.location.reload();
   }
 
 }
