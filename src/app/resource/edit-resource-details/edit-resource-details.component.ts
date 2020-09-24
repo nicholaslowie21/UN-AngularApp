9e128fc10d170e8887eb3c852ee6ee2f66db7db1
zhixuanlee <zhixuan.lee@u.nus.edu>
2020-09-24 21:25:59 +0800

    feat: added upload pic for item resource

--- src/app/resource/edit-resource-details/edit-resource-details.component.ts
+++ src/app/resource/edit-resource-details/edit-resource-details.component.ts
@@ -15,6 +15,12 @@ export class EditResourceDetailsComponent implements OnInit {
   isUpdateSuccessful = false;
   errorMessage = '';
 
+  itemImage: any;
+  isUploadItemPicSuccessful = false;
+  // currentVenueImage: any;
+  // isUploadVenuePicSuccessful = false;
+  errorMsgUploadPic = '';
+
   countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua and Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia and Herzegovina","Botswana","Brazil","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central African Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Côte d'Ivoire","Croatia","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","East Timor","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre and Miquelon","Samoa","San Marino","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","Saint Kitts and Nevis","Saint Lucia","Saint Vincent And The Grenadines","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Togo","Tonga","Trinidad and Tobago","Tunisia","Turkey","Turkmenistan","Turks and Caicos Islands","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam", "Virgin Islands (British)", "Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
 
   constructor(private route: ActivatedRoute, private resourceService: ResourceService) { }
@@ -37,6 +43,67 @@ export class EditResourceDetailsComponent implements OnInit {
       await this.resourceService.viewKnowledgeDetails({id: this.id}).toPromise().then(res => {this.resource = res.data.knowledge});
     }
   }
+
+  selectItemImage(event) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.itemImage = file;
+    }
+  }
+
+  onSubmitItemImage(): void {
+    if (this.itemImage == null) {
+      this.errorMsgUploadPic = 'Choose a file!';
+      this.isUploadItemPicSuccessful = false;
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("itemId", this.id);
+    formData.append("itemPic", this.itemImage);
+
+    this.resourceService.uploadItemPicture(formData).subscribe(
+      response => {
+        this.isUploadItemPicSuccessful = true;
+        this.reloadPage();
+      },
+      err => {
+        this.errorMsgUploadPic = err.error.msg;
+        this.isUploadItemPicSuccessful = false;
+      }
+    )
+  }
+
+  // selectVenueImage(event) {
+  //   if (event.target.files.length > 0) {
+  //     const file = event.target.files[0];
+  //     this.currentVenueImage = file;
+  //   }
+  // }
+
+  // onSubmitVenueImage(): void {
+  //   if (this.currentVenueImage == null) {
+  //     this.errorMsgUploadPic = 'Choose a file!';
+  //     this.isUploadVenuePicSuccessful = false;
+  //     return;
+  //   }
+
+  //   const formData = new FormData();
+  //   formData.append("venueId", this.id);
+  //   this.resource.imgPath.push(this.currentVenueImage);
+  //   formData.append("venuePics", this.resource.imgPath);
+
+  //   this.resourceService.uploadVenuePicture(formData).subscribe(
+  //     response => {
+  //       this.isUploadVenuePicSuccessful = true;
+  //       this.reloadPage();
+  //     },
+  //     err => {
+  //       this.errorMsgUploadPic = err.error.msg;
+  //       this.isUploadVenuePicSuccessful = false;
+  //     }
+  //   )
+  // }
 
   onSubmit(): void {
     console.log(this.id);
@@ -105,7 +172,10 @@ export class EditResourceDetailsComponent implements OnInit {
         }
       )
     }
-    
+  }
+
+  reloadPage(): void {
+    window.location.reload();
   }
 
 }
