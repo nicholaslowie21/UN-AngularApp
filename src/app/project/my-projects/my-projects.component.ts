ed49bb4925a0322ffbf0d0f6ef0d1dae6c357cce
Trudy <trudylim98@gmail.com>
2020-09-26 11:36:02 +0800

    add filter for past proj

--- src/app/project/my-projects/my-projects.component.ts
+++ src/app/project/my-projects/my-projects.component.ts
@@ -30,14 +30,14 @@ export class MyProjectsComponent implements OnInit {
   ngOnInit(): void {
     this.user = this.tokenStorageService.getUser();
     this.userId = this.user.id;
-    if(this.user.isVerified == "true") {
+    if (this.user.isVerified == "true") {
       this.isVerified = true;
     }
     this.filterOptions = [
-      {label: 'All', value: 'all'},
-      {label: 'Creator', value: 'creator'},
-      {label: 'Admin', value: 'admin'},
-      {label: 'Contributor', value: 'contributor'},
+      { label: 'All', value: 'all' },
+      { label: 'Creator', value: 'creator' },
+      { label: 'Admin', value: 'admin' },
+      { label: 'Contributor', value: 'contributor' },
     ];
     this.userService.getCurrentProjects({ id: this.userId }).subscribe(
       response => {
@@ -61,35 +61,35 @@ export class MyProjectsComponent implements OnInit {
     await this.userService.getCurrentProjects({ id: this.userId }).toPromise().then(res => this.currProj = res.data.currProjects);
     let value = event.value;
     let arr = [];
-    if(value == 'creator') {
-      for(var i=0; i<this.currProj.length; i++) {
-        if(this.currProj[i].host == this.user.id) {
+    if (value == 'creator') {
+      for (var i = 0; i < this.currProj.length; i++) {
+        if (this.currProj[i].host == this.user.id) {
           arr.push(this.currProj[i]);
         }
       }
-    } else if(value == 'admin') {
-      for(var i=0; i<this.currProj.length; i++) {
+    } else if (value == 'admin') {
+      for (var i = 0; i < this.currProj.length; i++) {
         console.log("check admin: " + this.currProj[i].admins);
-        for(var j=0; j<this.currProj[i].admins.length; j++) {
+        for (var j = 0; j < this.currProj[i].admins.length; j++) {
           console.log("***inner for loop")
-          if(this.currProj[i].admins[j] == this.user.id) {
+          if (this.currProj[i].admins[j] == this.user.id) {
             console.log("isAdmin HERE")
             arr.push(this.currProj[i]);
             break;
           }
         }
       }
-    } else if(value == 'contributor') {
+    } else if (value == 'contributor') {
       let isAdmin = false;
-      for(var i=0; i<this.currProj.length; i++) {
-        if(this.currProj[i].host != this.user.id) {
-          for(var j=0; j<this.currProj[i].admins.length; j++) {
-            if(this.currProj[i].admins[j] == this.user.id) {
+      for (var i = 0; i < this.currProj.length; i++) {
+        if (this.currProj[i].host != this.user.id) {
+          for (var j = 0; j < this.currProj[i].admins.length; j++) {
+            if (this.currProj[i].admins[j] == this.user.id) {
               isAdmin = true;
               break;
             }
           }
-          if(!isAdmin) {
+          if (!isAdmin) {
             arr.push(this.currProj[i]);
           }
         }
@@ -98,6 +98,49 @@ export class MyProjectsComponent implements OnInit {
       arr = this.currProj;
     }
     this.currProj = arr;
+  }
+
+  async filterRolePastProj(event) {
+    await this.userService.getPastProjects({ id: this.userId }).toPromise().then(res => this.pastProj = res.data.pastProjects);
+    let value = event.value;
+    let arr = [];
+    if (value == 'creator') {
+      for (var i = 0; i < this.pastProj.length; i++) {
+        if (this.pastProj[i].host == this.user.id) {
+          arr.push(this.pastProj[i]);
+        }
+      }
+    } else if (value == 'admin') {
+      for (var i = 0; i < this.pastProj.length; i++) {
+        console.log("check admin: " + this.pastProj[i].admins);
+        for (var j = 0; j < this.pastProj[i].admins.length; j++) {
+          console.log("***inner for loop")
+          if (this.pastProj[i].admins[j] == this.user.id) {
+            console.log("isAdmin HERE")
+            arr.push(this.pastProj[i]);
+            break;
+          }
+        }
+      }
+    } else if (value == 'contributor') {
+      let isAdmin = false;
+      for (var i = 0; i < this.pastProj.length; i++) {
+        if (this.pastProj[i].host != this.user.id) {
+          for (var j = 0; j < this.pastProj[i].admins.length; j++) {
+            if (this.pastProj[i].admins[j] == this.user.id) {
+              isAdmin = true;
+              break;
+            }
+          }
+          if (!isAdmin) {
+            arr.push(this.pastProj[i]);
+          }
+        }
+      }
+    } else {
+      arr = this.pastProj;
+    }
+    this.pastProj = arr;
   }
 
 }
