208b8ed77cbb3e52ccfc1a46ab320a2d6ea8e173
zevania <zevania@u.nus.edu>
2020-09-30 15:35:35 +0800

    Add filter in admin user mgmt

--- src/app/admin/admin-user-management/admin-user-management.component.ts
+++ src/app/admin/admin-user-management/admin-user-management.component.ts
@@ -28,6 +28,9 @@ export class AdminUserManagementComponent implements OnInit {
 
   checkUser: any;
 
+  filterCountryKey = 'Filter by country';
+  countries = ["All","Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua and Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia and Herzegovina","Botswana","Brazil","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central African Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Côte d'Ivoire","Croatia","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","East Timor","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre and Miquelon","Samoa","San Marino","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","Saint Kitts and Nevis","Saint Lucia","Saint Vincent And The Grenadines","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Togo","Tonga","Trinidad and Tobago","Tunisia","Turkey","Turkmenistan","Turks and Caicos Islands","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam", "Virgin Islands (British)", "Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+
   constructor(private adminService: AdminService, private tokenStorageService: TokenStorageService, 
     private projectService: ProjectService, private router: Router) { }
 
@@ -42,6 +45,21 @@ export class AdminUserManagementComponent implements OnInit {
     } else if (this.thisUser.role == 'admin') {
       this.isAdmin = true;
     }
+  }
+
+  async filterByCountry() {
+    let arr = [];
+    await this.searchUserProject();
+    if(this.filterCountryKey == 'Filter by country' || this.filterCountryKey == 'All') {
+      return;
+    } else {
+      for(var i=0; i<this.searchResults.length; i++) {
+        if(this.searchResults[i].country == this.filterCountryKey) {
+          arr.push(this.searchResults[i]);
+        }
+      }
+    }
+    this.searchResults = arr;
   }
 
   viewUser(user): void {
@@ -62,7 +80,7 @@ export class AdminUserManagementComponent implements OnInit {
     this.keyword = '';
   }
 
-  searchUserProject(): void {
+  async searchUserProject() {
     if (this.searchType == 'Select') {
       this.errorMsgSearch = 'Select search type';
       this.isSearchSuccessful = false;
@@ -75,7 +93,7 @@ export class AdminUserManagementComponent implements OnInit {
     }
 
     if (this.searchType == 'individual') {
-      this.adminService.searchUser({ username: this.keyword }).subscribe(
+      await this.adminService.searchUser({ username: this.keyword }).toPromise().then(
         response => {
           console.log(response.data.users);
           this.searchResults = response.data.users;
@@ -87,7 +105,7 @@ export class AdminUserManagementComponent implements OnInit {
         }
       )
     } else if (this.searchType == 'project') {
-      this.projectService.searchProject({ code: this.keyword }).subscribe(
+      await this.projectService.searchProject({ code: this.keyword }).toPromise().then(
         response => {
           this.searchResults = response.data.projects;
           this.isSearchSuccessful = true;
@@ -98,7 +116,7 @@ export class AdminUserManagementComponent implements OnInit {
         }
       )
     } else {
-      this.adminService.searchInstitution({ username: this.keyword }).subscribe(
+      await this.adminService.searchInstitution({ username: this.keyword }).toPromise().then(
         response => {
           this.searchResults = response.data.institutions;
           this.isSearchSuccessful = true;
