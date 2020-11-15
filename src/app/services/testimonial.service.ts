65b32df79078fbfcdab87b8b5122a85a9c7056b6
zevania <zevania@u.nus.edu>
2020-11-15 22:04:19 +0800

    add testimonial services

--- src/app/services/testimonial.service.ts
+++ src/app/services/testimonial.service.ts
@@ -14,4 +14,42 @@ const httpOptions = {
 export class TestimonialService {
 
   constructor(private http: HttpClient) { }
+
+  getCommonProjects(data): Observable<any> {
+    return this.http.get(API_URL + '/common/projects?accountId=' + data.id + '&accountType=' + data.type);
+  }
+
+  requestTestimonial(data): Observable<any> {
+    return this.http.post(API_URL + '/request', {
+      accountId: data.id,
+      accountType: data.type,
+      projectId: data.projectId
+    }, httpOptions);
+  }
+
+  getMyOutgoingTestimonials(data): Observable<any> {
+    return this.http.get(API_URL + '/outgoing?accountId=' + data.id + '&accountType=' + data.type 
+      + '&status=' + data.status);
+  }
+
+  getMyTestimonial(data): Observable<any> {
+    return this.http.get(API_URL + '?accountId=' + data.id + '&accountType=' + data.type 
+      + '&status=' + data.status);
+  }
+
+  updateMyTestimonialStatus(data): Observable<any> {
+    return this.http.post(API_URL + '/update/status', {
+      testimonialId: data.id,
+      status: data.status
+    }, httpOptions);
+  }
+
+  updateMyOutgoingTestimonial(data): Observable<any> {
+    return this.http.post(API_URL + '/outgoing/update/status', {
+      testimonialId: data.id,
+      status: data.status,
+      desc: data.desc
+    }, httpOptions);
+  }
+
 }
