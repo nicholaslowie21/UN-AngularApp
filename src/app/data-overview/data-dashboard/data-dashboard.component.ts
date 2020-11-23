7178c20afd43dd2e11d3f2374e88ac731be50c73
unknown <vennyl256@gmail.com>
2020-11-23 14:14:57 +0800

    Data dashboard UI

--- src/app/data-overview/data-dashboard/data-dashboard.component.ts
+++ src/app/data-overview/data-dashboard/data-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ɵɵqueryRefresh } from '@angular/core';
 import {ChartModule} from 'primeng/chart';
 
 @Component({
@@ -17,9 +17,12 @@ export class DataDashboardComponent implements OnInit {
   contributions: any;
   accBar: any;
   projectsLine: any;
+  years: any;
+  year: any;
 
 
   constructor() {
+    this.year = "All years";
    }
 
   ngOnInit() {
@@ -29,77 +32,165 @@ export class DataDashboardComponent implements OnInit {
     this.fundingNum= "2,999"; //Please return me in string, where there is commas?
     this.projectsOngoingNum= 122;
     this.projectsCompletedNum = 208;
-    this.resources = {
-      labels: ['Manpower','Item','Venue','Knowledge'],
-      datasets: [
-          {
-              data: [200, 150, 100, 12],
-              backgroundColor: [
-                  "#FF6384",
-                  "#36A2EB",
-                  "#FFCE56",
-                  "#eb803d"
-              ],
-              hoverBackgroundColor: [
-                  "#FF6384",
-                  "#36A2EB",
-                  "#FFCE56",
-                  "#eb803d"
-              ]
-          }]    
-      };
+    //Probably need to populate the years depending on until which year of data we have
+    this.years = ["All years", 2020, 2019, 2018];
+    this.initAllYears();
+  }
 
-      this.contributions = {
-        labels: ['Manpower','Item','Venue','Knowledge','Funding'],
+  initAllYears() {
+    this.resources = {
+        labels: ['Manpower','Item','Venue','Knowledge'],
         datasets: [
             {
-                data: [200, 150, 100, 12, 30],
+                data: [200, 150, 100, 12],
                 backgroundColor: [
                     "#FF6384",
                     "#36A2EB",
                     "#FFCE56",
-                    "#eb803d",
-                    "#2B9093"
+                    "#eb803d"
                 ],
                 hoverBackgroundColor: [
                     "#FF6384",
                     "#36A2EB",
                     "#FFCE56",
-                    "#eb803d",
-                    "#2B9093"
+                    "#eb803d"
                 ]
             }]    
-      };
-      
-      this.accBar = {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
-        datasets: [
-            {
-                label: 'User Accounts',
-                backgroundColor: '#42A5F5',
-                borderColor: '#1E88E5',
-                data: [65, 59, 80, 81, 56, 55, 40, 80, 81, 56, 55, 40]
-            },
-            {
-                label: 'Institution Accounts',
-                backgroundColor: '#9CCC65',
-                borderColor: '#7CB342',
-                data: [28, 48, 40, 19, 86, 27, 90, 28, 48, 40, 19, 86]
+        };
+  
+        this.contributions = {
+          labels: ['Manpower','Item','Venue','Knowledge','Funding'],
+          datasets: [
+              {
+                  data: [200, 150, 100, 12, 30],
+                  backgroundColor: [
+                      "#FF6384",
+                      "#36A2EB",
+                      "#FFCE56",
+                      "#eb803d",
+                      "#2B9093"
+                  ],
+                  hoverBackgroundColor: [
+                      "#FF6384",
+                      "#36A2EB",
+                      "#FFCE56",
+                      "#eb803d",
+                      "#2B9093"
+                  ]
+              }]    
+        };
+        
+        this.accBar = {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
+          datasets: [
+              {
+                  label: 'User Accounts',
+                  backgroundColor: '#42A5F5',
+                  borderColor: '#1E88E5',
+                  data: [65, 59, 80, 81, 56, 55, 40, 80, 81, 56, 55, 40]
+              },
+              {
+                  label: 'Institution Accounts',
+                  backgroundColor: '#9CCC65',
+                  borderColor: '#7CB342',
+                  data: [28, 48, 40, 19, 86, 27, 90, 28, 48, 40, 19, 86]
+              }
+          ]
+        }
+  
+        this.projectsLine = {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
+          datasets: [
+              {
+                  label: 'Projects',
+                  data: [65, 59, 80, 81, 56, 55, 40, 59, 80, 81, 56, 55],
+                  fill: false,
+                  borderColor: '#4bc0c0'
+              },
+          ]
+      }
+  }
+
+  update(event) {
+      console.log(event.target.value);
+      if (event.target.value == "All years") {
+        this.initAllYears();
+
+      } else {
+        //   Fetch data based on year passed in the drop down
+        this.resources = {
+            labels: ['Manpower','Item','Venue','Knowledge'],
+            datasets: [
+                {
+                    data: [0, 20, 10, 12],
+                    backgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56",
+                        "#eb803d"
+                    ],
+                    hoverBackgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56",
+                        "#eb803d"
+                    ]
+                }]    
+            };
+
+        this.contributions = {
+            labels: ['Manpower','Item','Venue','Knowledge','Funding'],
+            datasets: [
+                {
+                    data: [20, 15, 10, 12, 30],
+                    backgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56",
+                        "#eb803d",
+                        "#2B9093"
+                    ],
+                    hoverBackgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56",
+                        "#eb803d",
+                        "#2B9093"
+                    ]
+                }]    
+            };
+            
+            this.accBar = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
+            datasets: [
+                {
+                    label: 'User Accounts',
+                    backgroundColor: '#42A5F5',
+                    borderColor: '#1E88E5',
+                    data: [25, 9, 8, 1, 6, 5, 4, 18, 21, 5, 10, 4]
+                },
+                {
+                    label: 'Institution Accounts',
+                    backgroundColor: '#9CCC65',
+                    borderColor: '#7CB342',
+                    data: [28, 48, 40, 19, 86, 27, 90, 28, 48, 40, 19, 86]
+                }
+            ]
             }
-        ]
+    
+            this.projectsLine = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
+            datasets: [
+                {
+                    label: 'Projects',
+                    data: [15, 5, 8, 18, 6, 5, 4, 9, 8, 18, 5, 5],
+                    fill: false,
+                    borderColor: '#4bc0c0'
+                },
+            ]
+        }
       }
 
-      this.projectsLine = {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
-        datasets: [
-            {
-                label: 'Projects',
-                data: [65, 59, 80, 81, 56, 55, 40, 59, 80, 81, 56, 55],
-                fill: false,
-                borderColor: '#4bc0c0'
-            },
-        ]
-    }
   }
 
 }
--- src/app/data-overview/data-project/data-project.component.ts
+++ src/app/data-overview/data-project/data-project.component.ts
@@ -9,10 +9,26 @@ import {TableModule} from 'primeng/table';
 export class DataProjectComponent implements OnInit {
   dataBySDG: any;
   dataByCountry: any;
+  rangeDates: Date[];
+  minDateValue: any;
+  maxDateValue: any;
+  firstYear: any;
+  thisYear: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    // Set as date of the app creation
+    this.firstYear = 2018;
+    this.thisYear = 2020;
+    this.minDateValue = new Date();
+    this.minDateValue.setFullYear(this.firstYear);
+    this.minDateValue.setMonth(0);
+    this.maxDateValue = new Date();
+    this.initAll();
+  }
+
+  initAll() {
     this.dataBySDG = [
       { sdgTitle: '1: Alleviate Poverty', accountsNum:'100', projectsNum: '231', contributionsNum: '1231', fundingNum: '54406.00'},
       { sdgTitle: '2: Food', accountsNum:'200', projectsNum: '211', contributionsNum: '1231', fundingNum: '24406.00'},
@@ -28,4 +44,31 @@ export class DataProjectComponent implements OnInit {
     ]
   }
 
+  update(event) {
+    console.log(this.rangeDates);
+    // Will return me [Mon Nov 09 2020 00:00:00 GMT+0800 (Singapore Standard Time), Thu Nov 12 2020 00:00:00 GMT+0800 (Singapore Standard Time)]
+    if (this.rangeDates[1] != null) {
+      console.log("fetch data based on dates here and reinitialise table");
+        this.dataBySDG = [
+          { sdgTitle: '1: Alleviate Poverty', accountsNum:'10', projectsNum: '23', contributionsNum: '11', fundingNum: '506.00'},
+          { sdgTitle: '2: Food', accountsNum:'20', projectsNum: '21', contributionsNum: '11', fundingNum: '246.00'},
+          { sdgTitle: '3: Animal Protection', accountsNum:'30', projectsNum: '31', contributionsNum: '1', fundingNum: '46.00'},
+          { sdgTitle: '4: Water Safety', accountsNum:'40', projectsNum: '21', contributionsNum: '11', fundingNum: '16.00'},
+        ]
+    
+        this.dataByCountry = [
+          { country: 'Singapore', accountsNum:'10', projectsNum: '21', contributionsNum: '31', fundingNum: '546.00'},
+          { country: 'Indonesia', accountsNum:'20', projectsNum: '21', contributionsNum: '31', fundingNum: '206.00'},
+          { country: 'Malaysia', accountsNum:'30', projectsNum: '3', contributionsNum: '1', fundingNum: '46.00'},
+          { country: 'Vietnam', accountsNum:'40', projectsNum: '11', contributionsNum: '13', fundingNum: '146.00'},
+        ]
+    } else if (this.rangeDates[0] == null) {
+      this.initAll();
+    }
+  }
+
+  showAll(event) {
+    console.log("I should reinit to all");
+    this.initAll();
+  }
 }
