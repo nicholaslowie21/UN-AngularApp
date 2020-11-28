721f12d34a5113b55dd7796ec9e3c21a7dda567a
zevania <zevania@u.nus.edu>
2020-11-28 18:12:28 +0800

    Add timer to chat pop up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { TokenStorageService } from './services/token-storage.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CommunicationService } from './services/communication.service';
 import { MessageService } from 'primeng/api';
 import * as moment from 'moment';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -37,8 +38,19 @@ export class AppComponent implements OnInit {
   selectedChatType = '';
   totalUnreadRooms = 0;
 
+  source: any;
+  subscribe: any;
+
+  sourceRoom: any;
+  subscribeRoom: any;
+
+  @ViewChild('messageArea')
+  messageArea: ElementRef<HTMLElement>;
+
   constructor(private tokenStorageService: TokenStorageService, private route: ActivatedRoute,
     private router: Router, private communicationService: CommunicationService, private messageService: MessageService) {
+    this.source = interval(1000);
+    this.sourceRoom = interval(1000);
   }
 
   async ngOnInit() {
@@ -70,64 +82,6 @@ export class AppComponent implements OnInit {
       );
       await this.loadChat();
     }
-  }
-
-  async loadChat() {
-    this.chatStatus = this.tokenStorageService.getChatStatus();
-    console.log(this.chatStatus);
-
-    await this.communicationService.getFilteredChatRoomsList({ type: 'normal' }).toPromise().then(
-      res => this.chatRooms = res.data.chatRooms
-    );
-
-    await this.communicationService.getFilteredChatRoomsList({ type: 'admin' }).toPromise().then(
-      res => this.chatRoomsAdmin = res.data.chatRooms
-    );
-
-    console.log(this.chatRooms);
-    console.log(this.chatRoomsAdmin);
-
-    
-      if(this.chatStatus.selectedChatType == 'user') {
-        this.displayChatRooms = this.chatRooms;
-      } else {
-        this.displayChatRooms = this.chatRoomsAdmin;
-      }
-    
-
-    if (this.chatStatus.status == 'room') {
-      await this.communicationService.getChatMsgs({ id: this.chatStatus.id }).toPromise().then(
-        res => {
-          this.selectedChatImg = res.data.targetImg;
-          this.selectedChatRoom = res.data.chatRoom;
-          this.chatMsgs = res.data.chats;
-        }
-      );
-    }
-    console.log(this.selectedChatRoom);
-    console.log(this.chatMsgs);
-    this.countTotalUnreadRooms();
-  }
-
-  countTotalUnreadRooms() {
-    this.totalUnreadRooms = 0;
-    for(var i=0; i<this.chatRooms.length; i++) {
-      if(this.chatRooms[i].user1username == this.username && this.chatRooms[i].user1read == false) {
-        this.totalUnreadRooms += 1;
-      } else if(this.chatRooms[i].user2username == this.username && this.chatRooms[i].user2read == false) {
-        this.totalUnreadRooms += 1;
-      }
-    }
-
-    for(var i=0; i<this.chatRoomsAdmin.length; i++) {
-      if(this.chatRoomsAdmin[i].user1username == this.username && this.chatRoomsAdmin[i].user1read == false) {
-        this.totalUnreadRooms += 1;
-      } else if(this.chatRoomsAdmin[i].user2username == this.username && this.chatRoomsAdmin[i].user2read == false) {
-        this.totalUnreadRooms += 1;
-      }
-    }
-
-    return this.totalUnreadRooms;
   }
 
   onActivate(event: any): void {
@@ -165,30 +119,129 @@ export class AppComponent implements OnInit {
     window.location.reload();
   }
 
+  async loadChat() {
+    this.chatStatus = this.tokenStorageService.getChatStatus();
+    console.log(this.chatStatus);
+
+    await this.communicationService.getFilteredChatRoomsList({ type: 'normal' }).toPromise().then(
+      res => this.chatRooms = res.data.chatRooms
+    );
+
+    await this.communicationService.getFilteredChatRoomsList({ type: 'admin' }).toPromise().then(
+      res => this.chatRoomsAdmin = res.data.chatRooms
+    );
+
+    console.log(this.chatRooms);
+    console.log(this.chatRoomsAdmin);
+    
+    if(this.chatStatus.selectedChatType == 'user') {
+      this.displayChatRooms = this.chatRooms;
+    } else {
+      this.displayChatRooms = this.chatRoomsAdmin;
+    }
+
+    if(this.chatStatus.status == 'open') {
+      this.startTimerRoom();
+    }
+    if(this.chatStatus.status == 'room') {
+      await this.getMessages();
+      this.startTimer();
+    }
+
+    console.log(this.selectedChatRoom);
+    this.countTotalUnreadRooms();
+  }
+
+  async getMessages() {
+    await this.communicationService.getChatMsgs({ id: this.chatStatus.id }).toPromise().then(
+      res => {
+        this.selectedChatImg = res.data.targetImg;
+        this.selectedChatRoom = res.data.chatRoom;
+        this.chatMsgs = res.data.chats;
+      }
+    );
+  }
+
+  countTotalUnreadRooms() {
+    this.totalUnreadRooms = 0;
+    for(var i=0; i<this.chatRooms.length; i++) {
+      if(this.chatRooms[i].user1username == this.username && this.chatRooms[i].user1read == false) {
+        this.totalUnreadRooms += 1;
+      } else if(this.chatRooms[i].user2username == this.username && this.chatRooms[i].user2read == false) {
+        this.totalUnreadRooms += 1;
+      }
+    }
+
+    for(var i=0; i<this.chatRoomsAdmin.length; i++) {
+      if(this.chatRoomsAdmin[i].user1username == this.username && this.chatRoomsAdmin[i].user1read == false) {
+        this.totalUnreadRooms += 1;
+      } else if(this.chatRoomsAdmin[i].user2username == this.username && this.chatRoomsAdmin[i].user2read == false) {
+        this.totalUnreadRooms += 1;
+      }
+    }
+
+    return this.totalUnreadRooms;
+  }
+
+  async startTimer() {
+    if (this.subscribe) {
+      this.subscribe.unsubscribe();
+    }
+
+    this.subscribe = await this.source.subscribe(async () => {
+      console.log("timer is running");
+      await this.getMessages();
+    });
+  }
+
+  async startTimerRoom() {
+    if (this.subscribeRoom) {
+      this.subscribeRoom.unsubscribe();
+    }
+
+    this.subscribeRoom = await this.sourceRoom.subscribe(async () => {
+      console.log("room timer is running");
+      await this.loadChat();
+    });
+  }
+
   openChatPopUp(): void {
     this.tokenStorageService.setChatStatus({ status: "open", selectedChatType: "user" });
-    this.chatStatus = this.tokenStorageService.getChatStatus();
-    this.loadChat();
+    // this.chatStatus = this.tokenStorageService.getChatStatus();
+    // this.loadChat();
+    this.startTimerRoom();
   }
 
   closeChatPopUp(): void {
     this.tokenStorageService.setChatStatus({ status: "close" });
-    this.chatStatus = this.tokenStorageService.getChatStatus();
+    // this.chatStatus = this.tokenStorageService.getChatStatus();
     this.loadChat();
+    if (this.subscribe) {
+      this.subscribe.unsubscribe();
+    }
+    if (this.subscribeRoom) {
+      this.subscribeRoom.unsubscribe();
+    }
   }
 
-  openChatRoom(roomId): void {
+  async openChatRoom(roomId) {
     this.tokenStorageService.setChatStatus({ status: "room", id: roomId, selectedChatType: this.chatStatus.selectedChatType });
     this.chatStatus = this.tokenStorageService.getChatStatus();
-    this.loadChat();
+    // this.loadChat();
+    await this.startTimer();
+    this.scrollToLastMessage();
   }
 
   closeChatRoom(): void {
     if(this.selectedChatRoom.chatType == 'admin') this.chatStatus.selectedChatType = 'admin';
     else  this.chatStatus.selectedChatType = 'user';
     this.tokenStorageService.setChatStatus({ status: "open", selectedChatType: this.chatStatus.selectedChatType });
-    this.chatStatus = this.tokenStorageService.getChatStatus();
-    this.loadChat();
+    // this.chatStatus = this.tokenStorageService.getChatStatus();
+    // this.loadChat();
+    if (this.subscribe) {
+      this.subscribe.unsubscribe();
+    }
+    this.startTimerRoom();
   }
 
   onChangeChatType(): void {
@@ -204,6 +257,7 @@ export class AppComponent implements OnInit {
       response => {
         this.chatForm.message = "";
         this.loadChat();
+        this.scrollToLastMessage();
       }, err => {
         this.messageService.add({key:'toastMsg',severity:'error',summary:'Error',detail:err.error.msg});
       }
@@ -214,6 +268,13 @@ export class AppComponent implements OnInit {
     let formattedDate = moment(date).format("MMM DD");
     let formattedTime = moment(date).format("h:mm a");
     return formattedTime + ' | ' + formattedDate;
+  }
+
+  scrollToLastMessage() {
+    console.log("SCROLL");
+    if (this.messageArea) {
+      this.messageArea.nativeElement.scrollTop = this.messageArea.nativeElement.scrollHeight + 500;
+    }
   }
 
   sortFunction(a, b) {
